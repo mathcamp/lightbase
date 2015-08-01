c113f0f3fcdba5508182ee6968a750c3bb602473
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-31 18:50:21 -0700

    create workspace and playground

---
+++ HLDB-Demo/HLDB-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HLDB-Demo
+//
+//  Created by Andrew Breckenridge on 7/31/15.
+//  Copyright (c) 2015 Highlight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
--- HLDBTests/HLDBEntityTests.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HLDBEntityTests.swift
-//  HLDB
-//
-//  Created by Ben Garrett on 7/30/15.
-//  Copyright (c) 2015 Mathcamp. All rights reserved.
-//
-
-import Foundation
