8918dbe8a1ac865067af991d8fcbe6422235eb1f
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-03 15:34:23 -0700

    work on the demo project (still doesn't build

--- hldb-demo/HLDB-Demo/ViewController.swift
+++ hldb-demo/HLDB-Demo/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import hldb
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data: [String]? {
+        willSet {
+            
+        }
+        didSet {
+            tableView.reloadData()
+            if data!.count > 0 {
+                tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: data!.count - 1, inSection: 0), atScrollPosition: .Bottom, animated: true)
+            }
+        }
+    }
+    
+    let database = HLDB.DB(fileName: "superNecessaryDateDatabase")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func insertObjectsButtonWasHit(sender: UIButton) {
+        if let num = (split(sender.titleLabel!.text!) { $0 == " " }).last?.toInt() {
+            if data == nil {
+                data = [String]()
+            }
+            let start = NSDate()
+            data! += (0..<num).map { "\($0 + 1))" }
+        }
+    }
+}
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data?.count ?? 0
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        
+        cell.textLabel!.text = data![indexPath.row]
+        return cell
+        
+    }
 }
 
