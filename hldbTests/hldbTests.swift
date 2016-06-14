14c9c54c738a09193b6e5b6e0fc8b56eddc900fa
Benjamin Garrett <benagarr@gmail.com>
2016-06-14 13:43:02 -0700

    More test changes

--- hldbTests/hldbTests.swift
+++ hldbTests/hldbTests.swift
@@ -25,8 +25,16 @@ class hldbTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
       
+        let fields: [HLDB.Table.Field] = [
+          HLDB.Table.Field(name: "id",       type: .Text, index: .PrimaryKey, defaultValue: .NonNull),
+          HLDB.Table.Field(name: "ts",       type: .Real, index: .Index, defaultValue: .NonNull),
+          HLDB.Table.Field(name: "kind",     type: .Text, index: .None, defaultValue: .NonNull)
+        ]
       
         let db = DBModel()
+        let tableName = "mytable"
+        let table = db.getTable(tableName, fields: fields)
+      
         
     }
     
